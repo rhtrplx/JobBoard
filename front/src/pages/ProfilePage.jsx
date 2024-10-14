2a1c62fb01aeaa7fc85b9eea862f3cb28a8b7aa5
rhtrplx <rachid.lamkadem@epitech.eu>
2024-10-14 14:26:27 +0200

    display users data in profile

--- front/src/pages/ProfilePage.jsx
+++ front/src/pages/ProfilePage.jsx
@@ -5,14 +5,13 @@ import './Style.css';
 import NavigationHeader from '../components/Header';
 import { useNavigate } from 'react-router-dom';
 
-
 function ProfilePage() {
     const [userData, setUserData] = useState({
-        name: localStorage.getItem('username') || '',
-        lastname: localStorage.getItem('lastName') || '',
-        email: localStorage.getItem('email') || '',
-        description: localStorage.getItem('description') || '', 
-        title: localStorage.getItem('title') || ''
+        name: '',
+        lastname: '',
+        email: '',
+        description: '',
+        title: ''
     });
     const [loading, setLoading] = useState(true); // State to manage loading state
     const [error, setError] = useState(null); // State to handle errors
@@ -40,10 +39,13 @@ function ProfilePage() {
                 }
 
                 const data = await response.json();
-                setUserData((prevData) => ({
-                    ...prevData,
-                    ...data // Merge fetched data with existing state
-                }));
+                setUserData({
+                    name: data.user.name || '',
+                    lastname: data.user.lastname || '',
+                    email: data.user.email || '',
+                    description: data.user.description || '',
+                    title: data.user.title || ''
+                });
             } catch (error) {
                 console.error(error);
                 setError("Could not fetch user data");
@@ -69,23 +71,27 @@ function ProfilePage() {
             <div className="row">
                 <div className="col-md-4">
                     <div className="card profilecard d-flex justify-content-center align-items-center mb-3" style={{ height: '100%', borderStyle: 'dotted' }}>
-                        <img src={ProfilePhoto} className="card-img-center" alt="Profile Logo" style={{ width: '200px', height: '200px', objectFit: 'cover'}} />
+                        <img src={ProfilePhoto} className="card-img-center" alt="Profile Logo" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
                         <div className="card-body">
                             <h5 className="card-title d-flex justify-content-center align-items-center">{`${userData.name}`}</h5>
                             <h5 className="card-title d-flex justify-content-center align-items-center">{`${userData.title}`}</h5>
                             <p className="card-text d-flex justify-content-center align-items-center">
-                                Hi! My name is {userData.name || "..."}. 
-                                {userData.description || "..."}. 
-                                You can contact me at {userData.email || "..."}.
+                                {userData.name || "..."}.
+                            </p>
+                            <p className="card-text d-flex justify-content-center align-items-center">
+                                {userData.description || "..."}.
+                            </p>
+                            <p className="card-text d-flex justify-content-center align-items-center">
+                                {userData.email || "..."}.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-8">
                     <div className="row">
-                        <div className="col-12 mb-3"style={{ height: '100%'}}>
+                        <div className="col-12 mb-3" style={{ height: '100%' }}>
                             <div className="card">
-                                <div className="card-header" style={{ backgroundColor: '#386fa4', color:'white'}}>Saved Ads:</div>
+                                <div className="card-header" style={{ backgroundColor: '#386fa4', color: 'white' }}>Saved Ads:</div>
                                 <div className="card-body">
                                     <p>No saved ads yet.</p>
                                 </div>
@@ -93,11 +99,11 @@ function ProfilePage() {
                         </div>
                         <div className="col-12 mb-3">
                             <div className="card">
-                                <div className="card-header" style={{ backgroundColor: '#386fa4', color:'white'}}>Notes:</div>
+                                <div className="card-header" style={{ backgroundColor: '#386fa4', color: 'white' }}>Notes:</div>
                                 <div className="card-body d-flex flex-column" style={{ height: '100%' }}>
-                                    <textarea 
-                                        className="form-control" 
-                                        style={{ flex: 1 }} 
+                                    <textarea
+                                        className="form-control"
+                                        style={{ flex: 1 }}
                                         placeholder="Write your notes here..."
                                     />
                                 </div>
@@ -106,7 +112,7 @@ function ProfilePage() {
 
                         <div className="col-12 mb-3">
                             <div className="card" style={{ height: '100%' }}>
-                                <div className="card-header" style={{ backgroundColor: '#386fa4', color:'white'}}>Reminders</div>
+                                <div className="card-header" style={{ backgroundColor: '#386fa4', color: 'white' }}>Reminders</div>
                                 <div className="card-body">You are doing great! Keep on going.</div>
                             </div>
                         </div>
